62c3c9bcf6447635ba6b996ec0e07e9ee133a860
Dave Humphreys <newky2k@mac.com>
2019-10-14 16:08:27 +0100

    Fixed TouchEffect resoultion and add UWP support
    Update package notes

--- DSoft.XamarinForms.Controls/TouchTracking/TouchEffect.shared.cs
+++ DSoft.XamarinForms.Controls/TouchTracking/TouchEffect.shared.cs
@@ -5,9 +5,20 @@ namespace DSoft.XamarinForms.Controls.TouchTracking
 
     public class TouchEffect : RoutingEffect
     {
+        private const string effectId = "DSoft.XamarinForms.Controls.TouchEffect";
+
+        /// <summary>
+        /// Resolves the effect with its platform implementation
+        /// </summary>
+        /// <returns></returns>
+        public static Effect Build()
+        {
+            return Effect.Resolve(effectId);
+        }
+
         public event TouchActionEventHandler TouchAction;
 
-        public TouchEffect() : base("DSoft.XamarinForms.Controls.TouchTracking.TouchEffect") { }
+        public TouchEffect() : base(effectId) { }
 
         public bool Capture { set; get; }
 
